4367333fc63bcd8776dcee208d01fdd750a0ea6b
Abduvohid26 <abdujalilovabduvohid6@gmail.com>
2024-09-08 11:14:11 +0500

    fix

--- handlers/users/func.py
+++ handlers/users/func.py
@@ -1,10 +1,8 @@
 import aiohttp
 
 async def download_instagram(url):
-    api_url = "https://social-media-video-downloader.p.rapidapi.com/smvd/get/instagram"
-
+    api_url = "https://social-media-video-downloader.p.rapidapi.com/smvd/get/facebook"
     querystring = {"url": url}
-
     headers = {
         "x-rapidapi-key": "54e518fa11msha164dc2cecb21c8p18d479jsn65ee0a8c6b70",
         "x-rapidapi-host": "social-media-video-downloader.p.rapidapi.com"
@@ -12,13 +10,14 @@ async def download_instagram(url):
 
     async with aiohttp.ClientSession() as session:
         async with session.get(api_url, headers=headers, params=querystring) as response:
-            if response.status == 200:
+            content_type = response.headers.get('Content-Type', '')
+            if 'application/json' in content_type:
                 data = await response.json()
                 links = data.get('links', [])
                 if links:
-                    video_link = links[-1].get('link')
+                    video_link = links[0].get('link')
                     return video_link
                 else:
                     return "No video links found."
             else:
-                return f"Error: {response.status}"
+                return f"Unexpected content type: {content_type}"
