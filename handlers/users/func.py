c6eda60204c9afb8a2d0caaf04869bc46fe115ca
Abduvohid26 <abdujalilovabduvohid6@gmail.com>
2024-09-08 11:01:57 +0500

    fix

--- handlers/users/func.py
+++ handlers/users/func.py
@@ -2,7 +2,7 @@ import aiohttp
 async def download_instagram(url):
     url = "https://social-media-video-downloader.p.rapidapi.com/smvd/get/instagram"
 
-    querystring = {"url": "https://www.instagram.com/reel/C-lh9fTABT1/?utm_source=ig_web_copy_link"}
+    querystring = {"url": url}
 
     headers = {
         "x-rapidapi-key": "54e518fa11msha164dc2cecb21c8p18d479jsn65ee0a8c6b70",
